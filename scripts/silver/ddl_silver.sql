af2a550097dd412b0efe01157e3b645de2b540ec
Sudeb Kumar Mandal <127133814+Devrockzz654@users.noreply.github.com>
2025-06-09 07:22:10 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -20,7 +20,7 @@ CREATE TABLE silver.crm_cust_info (
     cst_key NVARCHAR(50),
     cst_first_name NVARCHAR(50),
     cst_last_name NVARCHAR(50),
-    cst_martial_status NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
     cst_gndr NVARCHAR(50),
     cst_create_date DATE,
     dwh_create_date DATETIME2 DEFAULT GETDATE()
